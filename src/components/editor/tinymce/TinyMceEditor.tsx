27c0c36afd2712f688e7cbf4b486765931932520
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-03-06 14:07:15 +0600

    added a button to get content

--- src/components/editor/tinymce/TinyMceEditor.tsx
+++ src/components/editor/tinymce/TinyMceEditor.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import {Editor} from "@tinymce/tinymce-react";
 
 import './TinyMceEditor.css'
@@ -12,16 +12,31 @@ interface Props{
 }
 */
 
-const TinyMceEditor = () => <Editor
-    id="---"
-    init={{
-        plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
-        menubar: 'file edit view insert format tools table',
-        toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
-        toolbar_sticky: true,
-        branding: false,
-        statusbar: false,
-    }}
-/>
+const TinyMceEditor = () => {
+
+        const ref=useRef<Editor|null>(null);
+        return(
+            <Editor
+                ref={ref}
+                init={{
+                        plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
+                        menubar: 'file edit view insert format tools table',
+                        toolbar: 'toBlog | undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+                        toolbar_sticky: true,
+                        branding: false,
+                        statusbar: false,
+                        setup: (editor => {
+                                editor.ui.registry.addButton('toBlog',{
+                                        text: "To Blog",
+                                        onAction: ()=>{
+                                                console.debug(ref.current?.editor?.getContent())
+                                        }
+                                })
+                        })
+                }}
+
+            />
+        )
+}
 
 export {TinyMceEditor}
