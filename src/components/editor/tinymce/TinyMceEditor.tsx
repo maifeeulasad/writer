ada7737c51f6c27634d5f27034bf462476dec23a
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-03-06 10:38:03 +0600

    [config]: homepage ref config to point to `./$` instead of `$`

--- src/components/editor/tinymce/TinyMceEditor.tsx
+++ src/components/editor/tinymce/TinyMceEditor.tsx
@@ -12,20 +12,16 @@ interface Props{
 }
 */
 
-const TinyMceEditor = () => {
-    return(
-        <Editor
-            id="---"
-            init={{
-                plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
-                menubar: 'file edit view insert format tools table help',
-                toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
-                toolbar_sticky: true,
-                branding: false,
-                statusbar: false,
-            }}
-        />
-    )
-}
+const TinyMceEditor = () => <Editor
+    id="---"
+    init={{
+        plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
+        menubar: 'file edit view insert format tools table help',
+        toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+        toolbar_sticky: true,
+        branding: false,
+        statusbar: false,
+    }}
+/>
 
 export {TinyMceEditor}
