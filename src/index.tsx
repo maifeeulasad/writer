1b66dd2b0c73754649bb76a54be28d128ed6e7ed
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-03-06 13:48:57 +0600

    added antd

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import {Editor} from "./components/editor";
 
+import 'antd/dist/antd.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <Editor />
