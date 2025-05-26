e372dd9dd49680fb97a57bb1fb2c2812b125f0a5
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-05-26 16:06:47 +0600

    [ui]: directly call ck and append data there;

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import { Editor } from './components/editor';
+import React, { useRef } from 'react';
+// import { Editor } from './components/editor';
 import { GenerateText } from './components/ai/GenerateText';
+import { CkEditor, CkEditorRef } from './components/editor/ck/CkEditor';
 
 const App = () => {
+    const editorRef = useRef<CkEditorRef>(null);
+
+    const enterOnEditor = (text: string) => {
+        console.log('Entering text into editor:', text);
+        editorRef.current?.appendData(text);
+    }
+
     return (
         <div>
-            <GenerateText />
-            <Editor />
+            <GenerateText enterOnEditor={enterOnEditor} />
+            <CkEditor ref={editorRef} />
         </div>
     );
 }
