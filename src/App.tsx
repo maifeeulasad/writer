73a0bc3451ee5b9afdbf94c72837f19b6e5ea883
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-05-26 22:14:23 +0600

    [feat]: download models on startup;

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { CkEditor, CkEditorRef } from './components/editor/ck/CkEditor';
 import { SummarizeText } from './components/ai/SummarizeText';
 import { GenerateText } from './components/ai/GenerateText';
+import { downloadModels } from './core/ai/InBrowserAi';
 
 const App = () => {
     const editorRef = useRef<CkEditorRef>(null);
@@ -16,6 +17,14 @@ const App = () => {
         }, 50);
 
         return () => clearInterval(checkEditor);
+    }, []);
+
+    const downloadInBrowserModels = async () => {
+        await downloadModels()
+    }
+
+    useEffect(() => {
+        downloadInBrowserModels();
     }, []);
 
     const enterOnEditor = (text: string) => {
