05ea44bda2cacdb9a57356ebe2c4897b28beb08e
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-05-28 12:30:34 +0600

    [core]: using editor js as default editor;

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { CkEditor, CkEditorRef } from './components/editor/ck/CkEditor';
-import { SummarizeText } from './components/ai/SummarizeText';
-import { GenerateText } from './components/ai/GenerateText';
-import { downloadModels } from './core/ai/InBrowserAi';
+import React from 'react';
+import { EditorJsEditor } from './components/editor/editorjs/EditorJsEditor';
 
 const App = () => {
-    const editorRef = useRef<CkEditorRef>(null);
-    const [editorReady, setEditorReady] = useState(false);
-
-    useEffect(() => {
-        const checkEditor = setInterval(() => {
-            if (editorRef.current) {
-                setEditorReady(true);
-                clearInterval(checkEditor);
-            }
-        }, 50);
-
-        return () => clearInterval(checkEditor);
-    }, []);
-
-    const downloadInBrowserModels = async () => {
-        await downloadModels()
-    }
-
-    useEffect(() => {
-        downloadInBrowserModels();
-    }, []);
-
-    const enterOnEditor = (text: string) => {
-        console.debug('Entering text into editor:', text);
-        editorRef.current?.appendData?.(text);
-    }
-
-    return (
-        <div>
-            <GenerateText enterOnEditor={enterOnEditor} />
-            {editorReady && <SummarizeText editor={editorRef.current} />}
-            <CkEditor ref={editorRef} />
-        </div>
-    );
+    return <EditorJsEditor />
 };
 
 export { App };
