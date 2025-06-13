74ab0ebc350323c134e32713fa2ddfb812c41e9d
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-06-13 20:22:55 +0600

    [ux]: show git hash in footer;

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ import { Footer as AntdFooter } from "antd/lib/layout/layout";
 const Footer = () => (
   <AntdFooter style={{ position: "sticky", bottom: "0" }}>
     <div style={{ textAlign: "center" }}>
-      &copy; {new Date().getFullYear()} - Maifee Ul Asad
+      &copy; {new Date().getFullYear()} - Maifee Ul Asad | Commit:{" "} <code>{process.env.REACT_APP_GIT_HASH}</code>
     </div>
   </AntdFooter>
 );
